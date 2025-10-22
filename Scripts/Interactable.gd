1e14593f11776f01f86f293a5a30bdf70ac12839
delingfengyu0711 <2978303864@qq.com>
2025-10-22 17:21:21 +0800

    Add Dialog system

--- Scripts/Interactable.gd
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fb92ee20fe035f8391ffc0ab06b49cde79eac7d23535fe65911364af61ba576d
-size 314
--- Scripts/Teleporter.gd
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3023287ad9867411fab34958ffe4106ded61f6f8dca49c73c3aa0654c38dd6f4
-size 244
