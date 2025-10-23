ade2d99212ac1f5e1b34f3f09ff4166350a3434e
delingfengyu0711 <2978303864@qq.com>
2025-10-23 17:52:40 +0800

    Add mini-game Data struct

--- Scripts/Tool/Items/FlagSwitch.gd
+++ Scripts/Tool/Items/FlagSwitch.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a49c1bc13daf48d1ccf206ba59cb3d5573d38665b6324d4e1017df964436866
-size 15
+oid sha256:f4738f64fd622783279d14f110ca860c935193a786e624e2f68e9e7dc8c74b0e
+size 546
