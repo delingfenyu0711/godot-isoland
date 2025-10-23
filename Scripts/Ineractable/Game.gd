fb191126c1e301d3671f2ebdbf98edc2fb5ea814
delingfengyu0711 <2978303864@qq.com>
2025-10-23 12:32:48 +0800

    Add item interactable scripts

---
+++ Scripts/Ineractable/Game.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d1c9db05d5a890e62cfbce5257139a081ce558b06dee7dc6fad28d533330786
+size 1337
---
+++ Scripts/Tool/Scene/Items/SceneItem.gd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d63e1b2efc004e71c10a3b6e5cffae7b673cadf2f4e1e76d05b7ace8d6b6b863
+size 872
